9e3cc8aed2150bf3f08c323a12e12a7cea1a571d
maximquantum <maximquantum148@gmail.com>
2024-06-07 18:42:47 +0200

    ArrayList

--- hc10-lists-sets-maps/src/Test.java
+++ hc10-lists-sets-maps/src/Test.java
@@ -1,13 +1,26 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-class Test {
+import org.junit.jupiter.api.BeforeEach;
 
-	Object[] myArray = {1, 2, 3};
+class Test {
 	
+	ArrayList myArray;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		myArray = new ArrayList(10);
+		myArray.add(1);
+		myArray.add(2);
+		myArray.add(3);
+	}
+	
+	@org.junit.jupiter.api.Test
 	void test() {
-		assertEquals( 1, myArray[0]);
-		myArray[1] = 16;
-		assertEquals( 16, myArray[1]);
+		assertEquals( 1, myArray.get(0));
+		myArray.set(1, 16);
+		myArray.add(2, 42);
+		assertEquals( 16, myArray.get(1));
+		assertEquals( 42, myArray.get(2));
 	}
 
 }
