65ce9d75aa945d95b16c0a125e5d0995e0854e14
maximquantum <maximquantum148@gmail.com>
2024-06-09 15:53:53 +0200

    crt ArraySet

---
+++ hc10-lists-sets-maps/src/ArraySet.java
@@ -0,0 +1,40 @@
+
+public class ArraySet implements Set {
+	/**
+	 * @representationObject
+	 * @invariant | a != null
+	 */
+	private ArrayList a;
+	
+	public ArraySet() {
+		a = new ArrayList(10);
+	}
+
+	@Override
+	public Object[] toArray() {
+		return a.toArray();
+	}
+
+	@Override
+	public int getSize() {
+		return a.getSize();
+	}
+
+	@Override
+	public void add(Object value) {
+		a.add(value);
+
+	}
+
+	@Override
+	public void remove(Object value) {
+		// a.remove(value);
+	}
+
+	@Override
+	public boolean contains(Object value) {
+		// return a.contains(value);
+		return false;
+	}
+
+}
